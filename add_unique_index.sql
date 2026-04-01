b039560e629e75cabc40ab4d99610aebe1412625
DERICKMALOBA <derickmaloba19@gmail.com>
2026-04-01 14:45:31 +0300

    removed

--- add_unique_index.sql
+++
@@ -1,4 +0,0 @@
--- Add unique index for C2B and B2C separation
-CREATE UNIQUE INDEX IF NOT EXISTS unique_active_tenant_service 
-ON tenant_mpesa_config (tenant_id, service_type) 
-WHERE is_active = true;
